1abdb819f9c3c6854afe9841876d7c735ad19262
FelipeFreitasDev <135486262+FelipeFreitasDev@users.noreply.github.com>
2023-06-26 23:31:16 -0300

    Project created

---
+++ backend/src/main/java/com/fullstack/ligaliga/LigaligaApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.ligaliga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LigaligaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LigaligaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fullstack/ligaliga/LigaligaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.ligaliga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LigaligaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
